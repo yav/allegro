695e84d7eeff94d8f5f4c12405db0a445b1cea65
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2013-11-24 13:02:12 -0800

    Bitmap example makes a window to match the loaded bitmap.

--- examples/c/bitmap.c
+++ examples/c/bitmap.c
@@ -2,7 +2,6 @@
 #include <allegro5/allegro_image.h>
 #include <stdio.h>
 
-
 int main(int argc, char *argv[]) {
 
   if (!al_init()) {
@@ -10,15 +9,8 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  ALLEGRO_DISPLAY *display = al_create_display(640,480);
-  if (display == NULL) {
-    fprintf(stderr, "Failed to create a display\n");
-    return 2;
-  }
-
   if (!al_init_image_addon()) {
     fprintf(stderr, "Failed to initialize image add-on\n");
-    al_destroy_display(display);
     return 3;
   }
 
@@ -26,11 +18,20 @@ int main(int argc, char *argv[]) {
   if (img == NULL) {
     fprintf(stderr, "Failed to load image.\n");
     al_shutdown_image_addon();
-    al_destroy_display(display);
     return 4;
   }
 
-  al_draw_tinted_bitmap(img, al_map_rgba_f(0.5, 0.5, 0.5, 0.5), 0, 0, 0);
+  int w = al_get_bitmap_width(img);
+  int h = al_get_bitmap_height(img);
+  printf("width = %d, height = %d\n", w, h);
+
+  ALLEGRO_DISPLAY *display = al_create_display(w,h);
+  if (display == NULL) {
+    fprintf(stderr, "Failed to create a display\n");
+    return 2;
+  }
+
+  al_draw_bitmap(img, 0,0,0);
   al_flip_display();
   al_rest(3);
 
